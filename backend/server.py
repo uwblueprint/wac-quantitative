cd8cea5d60c58affac551fbc28c7bd23922193a1
JerryXie98 <jerry_xie@hotmail.com>
2019-07-04 20:25:54 -0400

    Connecting frontend to backend.

--- backend/server.py
+++ backend/server.py
@@ -1,6 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, static_folder="../build/static", template_folder="../build")
+
+    @app.route("/")
+    def hello():
+        return render_template('index.html')
 
     return app
