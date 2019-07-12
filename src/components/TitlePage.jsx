950d9ec1abbdf03d21fc7631e09021711d72c43b
suzyng83209 <suzyn.one@gmail.com>
2019-07-11 20:46:32 -0400

    added shared components

---
+++ src/components/TitlePage.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Footer from "./Footer";
+
+import logo from "../images/largeLogo.png";
+import avatar from "../images/personIcon.png";
+import "../styles/_title_page.css";
+import "../styles/_buttons.css";
+
+const TitlePage = ({ resetData }) => {
+  return (
+    <div>
+      <div className="cardStyle">
+        <div className="introText">
+          <img style={{ marginBottom: "5%" }} src={logo} alt="Logo" />
+          <h3>Welcome to the We All Count Data Biography&nbsp;Builder.</h3>
+          <p>
+            This tool is designed around the Data Equity Biography checklist.
+            The tools helps you identify potential sources of bias in your
+            dataset and gives you the chance to improve the equity in your use
+            of the data.{" "}
+          </p>
+          <p>
+            This tool is designed to be used to assess the level of bias that is
+            present in dataset. This is designed to assess the level of bias
+            prior to analysis. To get a sense of the sources and locations of
+            potential bias in your data most completely you’ll need the a copy
+            of the survey tool that was used to collect the data, the codebook,
+            and the document that tells you how the data was sampled and
+            collected.
+          </p>
+          <button className="primary">
+            Begin
+          </button>
+          <div className="introPic">
+            <img src={avatar} alt="Avatar" />
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default TitlePage;
+
+TitlePage.propTypes = {
+  resetData: PropTypes.func
+};
