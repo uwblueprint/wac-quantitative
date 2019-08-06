1715226a128cef96bb27e174362364e6499531ed
Austin Jiang <austin.jiang@uwaterloo.ca>
2019-08-06 00:21:29 -0400

    functionality to handle csv input and return chi square results

---
+++ backend/algo/chisquare.py
@@ -0,0 +1,49 @@
+import numpy as np
+import pandas as pd
+import pickle
+
+from scipy.stats import chisquare
+
+def algo(path, cols, data):
+    df = pd.read_csv(path, index_col=None)
+    ret = []
+    for trait in data.keys():
+        obs = []
+        exp = []
+
+        for category, expected_value in data[trait].items():
+            for ind, val in df.loc[:, trait].value_counts().items():
+                if str(ind) == str(category):
+                    obs.append(val)
+
+            exp.append(expected_value)
+
+        test_statistic, p_value = chisquare(f_obs=obs, f_exp=exp, axis=None)
+        # print(trait, "test statistic:", test_statistic, "p-value:", p_value)
+
+        if p_value > 0.05:
+            ret.append("green")
+        elif p_value > 0.001:
+            ret.append("yellow")
+        else:
+            ret.append("red")
+    return ret
+
+# def main():
+#     path = 'csv/example_data.csv'
+#     cols = 3
+#     freq = {
+#         'Gender': {
+#             '1': 50,
+#             '2': 50,
+#         },
+#         'Ethnicity': {
+#             'white': 30,
+#             'black': 30,
+#             'other': 40,
+#         },
+#     }
+#     algo(path, cols, freq)
+
+# if __name__ == "__main__":
+#     main()
